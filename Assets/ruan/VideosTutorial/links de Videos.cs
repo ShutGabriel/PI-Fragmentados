6ae6a65a73071b8fa3e4499d77ae365bbed62df2
Ruan Sarmanho <ruan54382666@edu.pa.senac.br>
2024-09-05 16:45:12 -0300

    anima Yara - links

---
+++ Assets/ruan/VideosTutorial/links de Videos.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linksdeVideos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+//linkis 
+#region link para animação da yara
+// https://www.youtube.com/watch?v=yQiR2-0sbNw
+// https://www.youtube.com/watch?v=_6Jq1wliUVQ
+// https://www.youtube.com/watch?v=9hTnlp9_wX8&t=35s
+#endregion
