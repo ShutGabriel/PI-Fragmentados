697228bb3cfb8166fe195cfb802824d94b33017b
Ruan Sarmanho <ruan54382666@edu.pa.senac.br>
2024-10-10 15:44:41 -0300

    add player mapinguari

--- Assets/Daniel/Scripts/Atacks/ShootAttack.cs
+++ Assets/Daniel/Scripts/Atacks/ShootAttack.cs
@@ -17,60 +17,41 @@ public class ShootAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         Player = Camera.main.GetComponent<control>();
-
         speedactivate();
-
-
-
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         if (timerativado==true)
         {
-            Invoke("desativarbala",2);
+            Invoke("desativarbala",5);
             timerativado = false;
         }
-
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
+    {        
         if (collision.CompareTag("Inimigo"))
         { 
-         if (collision.gameObject.GetComponent<ScMapinguari>())
+            if (collision.gameObject.GetComponent<ScMapinguari>())
             {
-
                 collision.gameObject.GetComponent<ScMapinguari>().Dano();
-
+                desativarbala();
             }
-            
-            
-        
         }
-
 
     }
 
     public void desativarbala()
     {
-
         gameObject.SetActive(false);
     }
 
     public void speedactivate()
     {
-
-
         if (Player._player.transform.localScale.x > 0)
         {
             _rb.velocity = new Vector2(_speed, 0);
