4732e11b3a44b5f3065f3ac858d732b6f66db081
Gabriel Maciel <gabriel54108916@edu.pa.senac.br>
2024-10-24 17:16:45 -0300

    LA TROCA DE CENAS
    as vezes paro e penso que estou pensando em um mundo parado enquanto as pessoas pensam correndo enquanto correm.

--- Assets/gabriel/Script/MenuPrincipalManager.cs
+++ Assets/gabriel/Script/MenuPrincipalManager.cs
@@ -9,6 +9,7 @@ using TMPro;
 public class MenuPrincipalManager : MonoBehaviour
 {
     [SerializeField]
+    public GameObject[] button; 
     GameObject BT_Play;
     GameObject Bt_Options;
     GameObject BT_Quit;
@@ -20,17 +21,25 @@ public class MenuPrincipalManager : MonoBehaviour
 
     private void Start()
     {
+        for (int i = 0; i < button.Length; i++)
+        {
+            button[i].SetActive(true);
+        }
 
-        this.BT_Play.SetActive(true);
+       
         this.BarraProgresso.gameObject.SetActive(false);
-        this.mensagemTexto.gameObject.SetActive(false);
-
+       
     }
 
     public void jogar()
     {
-        this.BT_Play.SetActive(false);
+        for (int i = 0; i < button.Length; i++)
+        {
+            button[i].SetActive(false);
+        }
+
         this.BarraProgresso.gameObject.SetActive(true);
+        this.mensagemTexto.gameObject.SetActive(true);
         this.mensagemTexto.text = "carregando...";
         
         StartCoroutine(CarregarCena());
@@ -38,7 +47,7 @@ public class MenuPrincipalManager : MonoBehaviour
     }
     private IEnumerator CarregarCena()
     {
-        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Bagulho");
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Boss_Mapinguari");
         while (!asyncOperation.isDone)
         {
             this.BarraProgresso.value = asyncOperation.progress;
